4fa608f70583ac8c135438cd362a361ca4009e29
rawlingsj <rawlingsj80@gmail.com>
2017-10-24 10:23:46 +0100

    initial checkin

---
+++ Jenkinsfile
@@ -0,0 +1,27 @@
+#!/usr/bin/groovy
+@Library('github.com/fabric8io/fabric8-pipeline-library@master')
+def utils = new io.fabric8.Utils()
+
+releaseNode {  
+  checkout scm
+  readTrusted 'release.groovy'
+
+  if (utils.isCI()) {
+
+    mavenCI{}
+
+  } else if (utils.isCD()) {
+    sh "git remote set-url origin git@github.com:fabric8-apps/jenkins-test-app.git"
+
+    def pipeline = load 'release.groovy'
+    def stagedProject
+
+    stage('Stage') {
+      stagedProject = pipeline.stage()
+    }
+
+    stage('Promote') {
+      pipeline.release(stagedProject)
+    }
+  }
+}
---
+++ release.groovy
@@ -0,0 +1,34 @@
+#!/usr/bin/groovy
+def stage(){
+  return stageProject{
+    project = 'fabric8-apps/jenkins-test-app'
+    useGitTagForNextVersion = true
+  }
+}
+
+def approveRelease(project){
+  def releaseVersion = project[1]
+  approve{
+    room = null
+    version = releaseVersion
+    console = null
+    environment = 'fabric8'
+  }
+}
+
+def release(project){
+  releaseProject{
+    stagedProject = project
+    useGitTagForNextVersion = true
+    helmPush = false
+    groupId = 'io.fabric8.apps'
+    githubOrganisation = 'fabric8-apps'
+    artifactIdToWatchInCentral = 'jenkins-test-app'
+    artifactExtensionToWatchInCentral = 'pom'
+    promoteToDockerRegistry = 'docker.io'
+    dockerOrganisation = 'fabric8'
+    imagesToPromoteToDockerHub = []
+    extraImagesToTag = null
+  }
+}
+return this;
